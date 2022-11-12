018bf3d3dc5c3529129c6587a629fd0fb9bdd41d
evgeniia <evgeniia@saunalahti.fi>
2022-11-12 19:16:34 +0200

    tehtävä 11

--- viikko2/verkkokauppa-1/src/kauppa.py
+++ viikko2/verkkokauppa-1/src/kauppa.py
@@ -2,9 +2,9 @@ from ostoskori import Ostoskori
 
 class Kauppa:
     def __init__(self, varasto, pankki, viitegeneraattori):
-        self._varasto = varasto.get_instance()
-        self._pankki = pankki.get_instance()
-        self._viitegeneraattori = viitegeneraattori.get_instance()
+        self._varasto = varasto
+        self._pankki = pankki
+        self._viitegeneraattori = viitegeneraattori
         self._kaupan_tili = "33333-44455"
 
     def aloita_asiointi(self):
