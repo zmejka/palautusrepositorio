c77848021a1ae50d44bd674a186c105c5f386009
zmejka <evgeniia.minaeva@tutanota.com>
2022-11-24 21:17:10 +0200

    tehtävä 5, osa 5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,4 +26,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_summa_on_kahden_summa(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(), 8)        
 
