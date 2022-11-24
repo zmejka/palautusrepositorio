bff5bda6274e605c344b5db4fc868e0de7a25557
zmejka <evgeniia.minaeva@tutanota.com>
2022-11-24 21:37:48 +0200

    tehtävä 5, osa 8

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -1,6 +1,7 @@
 import unittest
 from ostoskori import Ostoskori
 from tuote import Tuote
+from ostos import Ostos
 
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
@@ -45,4 +46,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)        
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(len(self.kori.ostokset()), 1)
+        self.assertIsInstance(self.kori.ostokset()[0], Ostos)
+
 
